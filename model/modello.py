d568315b90e79da7932e6a8684b2cc82daa9ff1f
s298157 <s298157@studenti.polito.it>
2025-05-26 12:38:30 +0200

    Update

--- model/modello.py
+++ model/modello.py
@@ -10,6 +10,7 @@ class Model:
     def __init__(self):
         self._grafo = nx.DiGraph()
         self._nodes = []
+        # Inizializzazione delle variabili che serviranno nella determinazione del metodo ricorsivo
         self._cammino_ottimo = []
         self._score_ottimo = 0
 
@@ -24,7 +25,7 @@ class Model:
         self._nodes = DAO.get_nodes(year, shape)
         self._grafo.add_nodes_from(self._nodes)
 
-        # calcolo degli edges in modo programmatico
+        # Calcolo degli edges in modo programmatico
         for i in range(0, len(self._nodes) - 1):
             for j in range(i + 1, len(self._nodes)):
                 if self._nodes[i].state == self._nodes[j].state and self._nodes[i].longitude < self._nodes[j].longitude:
@@ -37,7 +38,6 @@ class Model:
     def get_top_edges(self):
         sorted_edges = sorted(self._grafo.edges(data=True), key=lambda edge: edge[2].get('weight'), reverse=True)
         return sorted_edges[0:5]
-
 
     def get_nodes(self):
         return self._grafo.nodes()
@@ -54,71 +54,56 @@ class Model:
     def cammino_ottimo(self):
         self._cammino_ottimo = []
         self._score_ottimo = 0
-
-        #TODO
         for node in self._grafo.nodes():
-            parziale = [node]
-            rimanenti = self.calcola_rimanenti(parziale)
-            self._ricorsione(parziale, rimanenti)
-
+            soluzione_parziale = [node]
+            nodi_rimanenti = self.calcola_rimanenti(soluzione_parziale)
+            self._ricorsione(soluzione_parziale, nodi_rimanenti)
         return self._cammino_ottimo, self._score_ottimo
 
-    def _ricorsione(self, parziale, nodi_rimanenti):
-        # caseo terminale:
+    def _ricorsione(self, soluzione_parziale, nodi_rimanenti):
+        # Caso terminale --> quando non si possono più aggiungere nodi
         if len(nodi_rimanenti) == 0:
-            punteggio = self.calcola_punteggio(parziale)
-            if punteggio > self._score_ottimo:
-                self._score_ottimo = punteggio
-                self._cammino_ottimo = copy.deepcopy(parziale)
-            print(parziale)
-        #caso ricorsivo
+            print(soluzione_parziale)
+        # Caso ricorsivo:
         else:
-            #per ogni nodo rimanente
+            # per ogni nodo rimanente, bisogna:
             for nodo in nodi_rimanenti:
-                # aggiungere il nodo
-                parziale.append(nodo)
-                #calcolare i nuovi rimanenti di questo nodo
-                nuovi_rimanenti = self.calcola_rimanenti(parziale)
-                # andare avanti nella ricorsione
-                self._ricorsione(parziale, nuovi_rimanenti)
-                #backtracking
-                parziale.pop()
+                # 1. aggiungere il nodo
+                soluzione_parziale.append(nodo)
+                # 2. calcolare i nuovi nodi rimanenti del nodo appena aggiunto
+                nuovi_rimanenti = self.calcola_rimanenti(soluzione_parziale)
+                # 3. proseguire con la ricorsione
+                self._ricorsione(soluzione_parziale, nuovi_rimanenti)
+                # 4. fare il backtracking
+                soluzione_parziale.pop()
+
 
     def calcola_punteggio(self, parziale):
-        punteggio = 0
-        # termine fisso
-        punteggio += 100*len(parziale)
-        #termine variabile
-        for i in range(1, len(parziale)):
-            nodo = parziale[i]
-            nodo_precedente = parziale[i-1]
-            if nodo.datetime.month == nodo_precedente.datetime.month:
-                punteggio +=200
-        #return
-        return punteggio
+        pass
 
-    def calcola_rimanenti(self, parziale):
+    def calcola_rimanenti(self, soluzione_parziale): # --> calcola i nodi rimanenti rispetto al nodo che si sta
+    # visitando
         nuovi_rimanenti = []
-        # prendiamo i nodi successivi
-        for i in self._grafo.successors(parziale[-1]):
-            # di questi nodi, dobbiamo verificare il vincolo sul mese
-            if (self.is_vincolo_ok(parziale, i) and
-                    self.is_vincolo_durata_ok(parziale, i)):
+        for i in self._grafo.successors(soluzione_parziale[-1]): # --> prende i nodi successivi
+            # presi i nodi, bisogna verificare il vincolo sul mese: non ci possono essere più di tre avvistamenti per
+            # uno stesso mese... e il vincolo sulla durata: la durata degli avvistamenti deve essere crescente
+            if self.is_vincolo_ok(soluzione_parziale, i) and self.is_vincolo_durata_ok(soluzione_parziale, i):
                 nuovi_rimanenti.append(i)
+        # --> bisogna fare questo piccolo ciclo perché altrimenti verrebbe restituito un iteratore che non possiede
+        # la funzione relativa alla lunghezza
         return nuovi_rimanenti
 
-    def is_vincolo_durata_ok(self, parziale, nodo:Sighting):
-        return nodo.duration>parziale[-1].duration
+    def is_vincolo_durata_ok(self, soluzione_parziale, nodo:Sighting):
+        return nodo.duration > soluzione_parziale[-1].duration
 
-    def is_vincolo_ok(self, parziale, nodo:Sighting):
+    def is_vincolo_ok(self, soluzione_parziale, nodo:Sighting): # l'aggiunta sulla variabile nodo perché di rendere
+    # l'oggetto tipizzato come un Sighting
         mese = nodo.datetime.month
         counter = 0
-        for i in parziale:
+        for i in soluzione_parziale:
             if i.datetime.month == mese:
-                counter += 1
-        if counter >=3:
+                counter = counter + 1
+        if counter >= 3:
             return False
         else:
             return True
-
-
